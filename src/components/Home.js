7d518c782dfdc33d36d50ae3eaa5982ca8d41337
Marina Brunello <marina_brunello@ajg.com>
2021-11-09 13:47:24 +0000

    Add login feature

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,7 +10,6 @@ import SearchBar from "./SearchBar";
 import Button from "./Button";
 //Hook
 import { useHomeFetch } from "../hooks/useHomeFetch";
-
 //Image
 import NoImage from "../images/no_image.jpg";
 
